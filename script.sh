df3dc2b048009318e5b151daafa805993ed9ec4e
yogeshraheja <yogesh.10d@gmail.com>
2018-02-17 16:41:11 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "STARTING MYSECOND PROJECT FROM GIT PUGIN"
+echo "########################################"
+mkdir /var/tmp/secondproject
+cd /var/tmp/secondproject
+touch abc
+touch def
+touch ghi
+ls -lrt 
+echo "ENDING THE PROJECT USED FROM GIT"
